ead385cb86e590c273c6d90556f6029dd8dd0369
Raphael Bouendeu <raphael.bouendeu@itbcafrica.com>
2021-08-08 14:07:12 +0200

    added new features

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+before_action :authenticate_user!
+before_action :only_current_user
 
 def new
   @user=User.find(params[:user_id])
@@ -22,13 +24,25 @@ def edit
 end
 
 def update
-
+  @user=User.find(params[:user_id])
+  @profile=@user.profile
+  if @profile.update(profile_params)
+     flash[:success]="Profile Updated"
+     redirect_to user_path(params[:user_id])
+  else
+    render action: :edit
+  end
 end
 
 private
 
 def profile_params
-  params.require(:profile).permit(:first_name,:last_name,:job_title,:phone_number,:contact_email,:description)
+  params.require(:profile).permit(:first_name,:last_name,:job_title,:phone_number,:contact_email,:description,:image)
+end
+
+def only_current_user
+@user =User.find(params[:user_id])
+redirect_to root_path unless @user==current_user
 end
 
 end
